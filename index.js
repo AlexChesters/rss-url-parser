9afbce194a80376d3ad37ae75ce5934be328ab18
Alex Chesters <alex.chesters@bbc.co.uk>
2016-05-16 09:23:48 +0100

    Add fetcher

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-module.exports = () => () => 'Hello, world!'
+'use strict'
+
+const FeedParser = require('feedparser')
+const request = require('request')
+
+module.exports = function (url) {
+  return new Promise(function (resolve, reject) {
+    let events = []
+
+    let req = request(url)
+    let parser = new FeedParser()
+
+    req.on('error', (err) => {
+      return reject(err)
+    })
+
+    req.on('response', (res) => {
+      if (res.statusCode !== 200) {
+        return reject({message: `Bad status code: ${res.statusCode}`})
+      }
+      return req.pipe(parser)
+    })
+
+    parser.on('error', (err) => {
+      return reject(err)
+    })
+
+    parser.on('end', () => {
+      return resolve(events)
+    })
+
+    parser.on('readable', () => {
+      let item = parser.read()
+
+      while (item) {
+        events.push(item)
+        // If there is no next item, parser.read() will return null, breaking the loop
+        item = parser.read()
+      }
+    })
+  })
+}
