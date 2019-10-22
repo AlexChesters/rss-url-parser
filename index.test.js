9cfe9e22a9820ea039b1fa952c9095d2b5d4d20b
Alex Chesters <alexchesters0@gmail.com>
2019-10-22 01:09:07 +0100

    add another test case

--- index.test.js
+++ index.test.js
@@ -23,4 +23,13 @@ describe('rss-url-parser', () => {
       'Bad status code: 404'
     ))
   })
+  test('rejects if the response is not valid XML', async () => {
+    nock('https://foo.com')
+      .get('/')
+      .reply(200, '<h1>Some html</h1>')
+
+    await expect(parser('https://foo.com')).rejects.toEqual(new Error(
+      'Not a feed'
+    ))
+  })
 })
