725b08227a5bc90498c5c55a9b61c796e4f94be8
Alex Chesters <alexchesters0@gmail.com>
2019-10-22 01:07:36 +0100

    add tests:

---
+++ index.test.js
@@ -0,0 +1,26 @@
+/* eslint-env jest */
+
+const nock = require('nock')
+
+const parser = require('./')
+
+describe('rss-url-parser', () => {
+  test('rejects when an error occurs during the request', async () => {
+    nock('https://foo.com')
+      .get('/')
+      .replyWithError({ message: 'something awful happened' })
+
+    await expect(parser('https://foo.com')).rejects.toEqual({
+      message: 'something awful happened'
+    })
+  })
+  test('rejects when a non-200 status code is received', async () => {
+    nock('https://foo.com')
+      .get('/')
+      .reply(404)
+
+    await expect(parser('https://foo.com')).rejects.toEqual(new Error(
+      'Bad status code: 404'
+    ))
+  })
+})
