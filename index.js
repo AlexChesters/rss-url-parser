b6fab751a5cc88743d5a257ed7b5ecb033505bf4
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2020-06-24 10:03:34 +0100

    use node-fetch to fetch feed

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
 const FeedParser = require('feedparser')
-const request = require('request')
+const fetch = require('node-fetch')
 
 module.exports = (url) => {
   return new Promise((resolve, reject) => {
     const events = []
 
-    const req = request(url)
     const parser = new FeedParser()
 
-    req.on('error', (err) => {
-      return reject(err)
-    })
-
-    req.on('response', (res) => {
-      if (res.statusCode !== 200) {
-        return reject(new Error(`Bad status code: ${res.statusCode}`))
-      }
-      return req.pipe(parser)
-    })
+    fetch(url)
+      .then((res) => {
+        if (!res.ok) {
+          return reject(new Error(`Bad status code: ${res.status}`))
+        }
+        res.body.pipe(parser)
+      })
+      .catch((err) => reject(err))
 
     parser.on('error', (err) => {
       return reject(err)
